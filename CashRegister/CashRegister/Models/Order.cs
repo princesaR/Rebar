67822cdd94bc3d83a7e79672b81115049e8ffdbb
Reut <reutprober100@gmail.com>
2023-10-30 11:24:01 +0200

    Added order class.

---
+++ CashRegister/CashRegister/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿namespace CashRegister.Models
+{
+    public class Order
+    {
+        private string _id;
+        private Dictionary<Shake, double> _shakes;
+        private double _totalPrice;
+        private string _clientName;
+        private DateTime _date;
+        private double _discounts;
+
+        public double TotalPrice
+        {
+            get { return _totalPrice; }
+        }
+        public Order(Dictionary<Shake, double> shakes, double price, string clientName, DateTime date, double discounts)
+        {
+            _id = new Guid().ToString();
+            _shakes = shakes;
+            _totalPrice = price;
+            _clientName = clientName;
+            _date = date;
+            _discounts = discounts;
+        }
+    }
+}
