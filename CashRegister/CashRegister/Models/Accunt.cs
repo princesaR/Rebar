e68c7f80594601a74137a5df4a7fd95aaf328d07
Reut <reutprober100@gmail.com>
2023-10-30 11:24:31 +0200

    Added Accunt class.

---
+++ CashRegister/CashRegister/Models/Accunt.cs
@@ -0,0 +1,19 @@
+﻿namespace CashRegister.Models
+{
+    public class Accunt
+    {
+        public List<Order> orders;
+        public double totalPrice;
+
+        public Accunt()
+        {
+            orders = new List<Order>();
+        }
+
+        public void AddOrder(Order order)
+        {
+            orders.Add(order);
+            totalPrice += order.TotalPrice;
+        }
+    }
+}
