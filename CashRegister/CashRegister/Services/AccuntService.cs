bc8bd50003a14ea15ad54aa26ecf5f39f3579990
Reut <reutprober100@gmail.com>
2023-10-30 22:23:51 +0200

    Added AccuntService.cs. Implements an interface.

---
+++ CashRegister/CashRegister/Services/AccuntService.cs
@@ -0,0 +1,41 @@
+﻿using CashRegister.Models;
+using MongoDB.Driver;
+
+namespace CashRegister.Services
+{
+    public class AccuntService : IAccuntService
+    {
+        private readonly IMongoCollection<Accunt> _accunts;
+
+        public AccuntService(IRebarDatabaseSettings setting, IMongoClient mongoClient)
+        {
+            var dataBase = mongoClient.GetDatabase(setting.DatabaseName);
+            var _accunts = dataBase.GetCollection<Shake>(setting.ShakesCollectionName);
+        }
+       
+        public async Task<List<Accunt>> GetAsync()
+        {
+            return await _accunts.Find(_ => true).ToListAsync();
+        }
+
+        public async Task<Accunt?> GetAsync(string id)
+        {
+            return await _accunts.Find(x => x.Id == id).FirstOrDefaultAsync();
+        }
+        public async Task CreateAsync(Accunt newAccunt)
+        {
+            await _accunts.InsertOneAsync(newAccunt);
+        }
+
+
+        public async Task UpdateAsync(string id, Accunt updatedAccunt)
+        {
+            await _accunts.ReplaceOneAsync(x => x.Id == id, updatedAccunt);
+        }
+
+        public async Task RemoveAsync(string id)
+        {
+            await _accunts.DeleteOneAsync(x => x.Id == id);
+        }
+    }
+}
