11c16f4fc202b69d8a812244913696ee48201666
Reut <reutprober100@gmail.com>
2023-10-30 22:24:25 +0200

    Added ShakeService.cs. Implements an interface.

---
+++ CashRegister/CashRegister/Services/ShakeService.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Driver;
+using CashRegister.Models;
+
+
+namespace CashRegister.Services
+{
+    public class ShakeService : IShakeService
+    {
+
+        private readonly IMongoCollection<Shake> _shakes;
+
+       
+        public ShakeService(IRebarDatabaseSettings setting, IMongoClient mongoClient)
+        {
+            var dataBase = mongoClient.GetDatabase(setting.DatabaseName);
+            var _shakes = dataBase.GetCollection<Shake>(setting.ShakesCollectionName);
+        }
+
+
+        public async Task<List<Shake>> GetAsync() =>
+        await _shakes.Find(_ => true).ToListAsync();
+
+        public async Task<Shake?> GetAsync(string id) =>
+        await _shakes.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(Shake newShake) =>
+            await _shakes.InsertOneAsync(newShake);
+
+        public async Task UpdateAsync(string id, Shake updatedShake) =>
+            await _shakes.ReplaceOneAsync(x => x.Id == id, updatedShake);
+
+        public async Task RemoveAsync(string id) =>
+            await _shakes.DeleteOneAsync(x => x.Id == id);
+
+    }
+}
