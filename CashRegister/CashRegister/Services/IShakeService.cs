e5cf24836f1321dd94ab1dab384a1717fc3640ec
Reut <reutprober100@gmail.com>
2023-10-30 22:21:35 +0200

    Added IShakeService.cs. in Services folder.

---
+++ CashRegister/CashRegister/Services/IShakeService.cs
@@ -0,0 +1,14 @@
+﻿using CashRegister.Models;
+
+namespace CashRegister.Services
+{
+    public interface IShakeService
+    {
+        
+        Task<List<Shake>> GetAsync();
+        Task<Shake?> GetAsync(string id);
+        Task CreateAsync(Shake newShake);
+        Task UpdateAsync(string id, Shake updatedShake);
+        Task RemoveAsync(string id);
+    }
+}
