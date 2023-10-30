b94671a067575871b2a820b9e468a077366e2ebe
Reut <reutprober100@gmail.com>
2023-10-30 22:22:38 +0200

    Added IAccuntService.cs. in Services folder.

---
+++ CashRegister/CashRegister/Services/IAccuntService.cs
@@ -0,0 +1,15 @@
+﻿using CashRegister.Models;
+
+namespace CashRegister.Services
+{
+    public interface IAccuntService
+
+    {
+        Task<List<Accunt>> GetAsync();
+        Task<Accunt?> GetAsync(string id);
+        Task CreateAsync(Accunt newAccunt);
+        Task UpdateAsync(string id, Accunt updatedAccunt);
+        Task RemoveAsync(string id);
+
+    }
+}
