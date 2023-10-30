40e6a2f414d82109263369cea635440ffa0ea306
Reut <reutprober100@gmail.com>
2023-10-30 22:27:36 +0200

    Modified Accunt class.

--- CashRegister/CashRegister/Models/Accunt.cs
+++ CashRegister/CashRegister/Models/Accunt.cs
@@ -1,14 +1,21 @@
-﻿namespace CashRegister.Models
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace CashRegister.Models
 {
     public class Accunt
     {
+        [BsonElement("orders")]
         public List<Order> orders;
+
+        [BsonElement("totalPrice")]
         public double totalPrice;
 
         public Accunt()
         {
             orders = new List<Order>();
         }
+
+        public string Id { get; internal set; }
 
         public void AddOrder(Order order)
         {
