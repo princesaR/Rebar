802b4d0e145eec14e63a645bcedf8fad3ae1d3b1
Reut <reutprober100@gmail.com>
2023-10-30 22:27:54 +0200

    Modified Order class.

--- CashRegister/CashRegister/Models/Order.cs
+++ CashRegister/CashRegister/Models/Order.cs
@@ -1,19 +1,44 @@
-﻿namespace CashRegister.Models
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace CashRegister.Models
 {
     public class Order
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         private string _id;
+
+        [BsonElement("orderedShakes")]
         private Dictionary<Shake, double> _shakes;
+             
+
+        [BsonElement("totalPrice")]
         private double _totalPrice;
+
+        [BsonElement("clientName")]
         private string _clientName;
+
+        [BsonElement("date")]
         private DateTime _date;
-        private double _discounts;
+
+        [BsonElement("creationTime")]
+        private DateTime _creationTime;
+
+        [BsonElement("endTime")]
+        private DateTime _endTime;
+
+        [BsonElement("discounts")]
+        private List<Discount> _discounts;
 
         public double TotalPrice
         {
             get { return _totalPrice; }
         }
-        public Order(Dictionary<Shake, double> shakes, double price, string clientName, DateTime date, double discounts)
+
+        public string Id { get; internal set; }
+
+        public Order(Dictionary<Shake, double> shakes, double price, string clientName, DateTime date, List<Discount> discounts)
         {
             _id = new Guid().ToString();
             _shakes = shakes;
