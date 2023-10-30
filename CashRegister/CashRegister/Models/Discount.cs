f6a5da966682660417619c36aab30d102c8dde40
Reut <reutprober100@gmail.com>
2023-10-30 22:35:02 +0200

    Added discount class.

---
+++ CashRegister/CashRegister/Models/Discount.cs
@@ -0,0 +1,18 @@
+﻿namespace CashRegister.Models
+{
+    public class Discount
+    {
+        private string _id;
+        private double _percentage;
+        private string _discription;
+
+        public Discount(double percentage, string description)
+        {
+            _id = new Guid().ToString();
+            _percentage = percentage;
+            _discription = description;
+        }
+
+    }
+    
+}
