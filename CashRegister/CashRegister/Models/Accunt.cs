8058295ba4a40fe9a6801313b324a771691552d7
Reut <reutprober100@gmail.com>
2023-10-31 14:44:13 +0200

    Fixed constructor, add Bson attribute to properties.

--- CashRegister/CashRegister/Models/Accunt.cs
+++ CashRegister/CashRegister/Models/Accunt.cs
@@ -4,21 +4,33 @@ namespace CashRegister.Models
 {
     public class Accunt
     {
+       
+        private string _id;
+        private List<Order> _orders;
+        private double _totalPrice;
+        private DateTime _day;
+
         [BsonElement("id")]
-        private string _id; 
+        public string Id { get { return _id; } }
 
         [BsonElement("orders")]
-        private List<Order> orders;
+        public List<Order> Orders { get { return _orders; } }
 
         [BsonElement("totalPrice")]
-        private double totalPrice;
+        public double TotalPrice { get { return _totalPrice; } }
+
+        [BsonElement("day")]
+        public DateTime Day { get { return _day; } }
 
         public Accunt()
         {
-            orders = new List<Order>();
+            _orders = new List<Order>();
+            _totalPrice = 0;
+            _day = DateTime.Now;
         }
 
-        public string Id { get; internal set; }
+        
+        
 
        
     }
