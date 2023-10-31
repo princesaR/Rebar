ad6b2b7a51fbf285c1d2b66bbf2c07f10ff02147
Reut <reutprober100@gmail.com>
2023-10-31 09:41:21 +0200

    Added folder data with MongodbRebarContent to connect the db.

---
+++ CashRegister/CashRegister/Data/MongodbRebarContext.cs
@@ -0,0 +1,31 @@
+﻿using CashRegister.Models;
+using MongoDB.Driver;
+
+namespace CashRegister.Data
+{
+    public class MongodbRebarContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongodbRebarContext(string connectionString, string databaseName)
+        {
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase(databaseName);
+        }
+
+        public IMongoCollection<Shake> Shakes()
+        {
+           return _database.GetCollection<Shake>("Shakes");
+        } 
+
+        public IMongoCollection<Order> Orders()
+        {
+           return _database.GetCollection<Order>("Orders");
+        }
+
+        public IMongoCollection<Accunt> Accunts()
+        {
+            return _database.GetCollection<Accunt>("Accunts");
+        }
+    }
+}
