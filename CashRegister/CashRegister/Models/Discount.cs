25f15af8f47a1183b28661887af7609bda56c2d6
Reut <reutprober100@gmail.com>
2023-10-31 14:44:25 +0200

    Fixed constructor, add Bson attribute to properties.

--- CashRegister/CashRegister/Models/Discount.cs
+++ CashRegister/CashRegister/Models/Discount.cs
@@ -1,4 +1,6 @@
-﻿namespace CashRegister.Models
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace CashRegister.Models
 {
     public class Discount
     {
@@ -6,9 +8,18 @@
         private double _percentage;
         private string _discription;
 
+        [BsonElement("discountId")]
+        public string Id { get { return _id; } }
+
+        [BsonElement("precentage")]
+        public double Percentage { get { return _percentage; } }
+
+        [BsonElement("discription")]
+        public string Discription { get { return _discription; } }
+
         public Discount(double percentage, string description)
         {
-            _id = new Guid().ToString();
+            _id = Guid.NewGuid().ToString();
             _percentage = percentage;
             _discription = description;
         }
