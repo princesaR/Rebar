6b99f9edbeba2c0c87dc84640fcd2991cfb0d859
Reut <reutprober100@gmail.com>
2023-10-31 14:22:58 +0200

    Fixed constructor, add Bson attribute to properties.

--- CashRegister/CashRegister/Models/Order.cs
+++ CashRegister/CashRegister/Models/Order.cs
@@ -1,60 +1,54 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson;
+using System.ComponentModel.DataAnnotations;
 
 namespace CashRegister.Models
 {
     public class Order
     {
-        [BsonElement("id")]
+      
         private string _id;
-
-        [BsonElement("orderedShakes")]
         private List<OrderedShake> _shakes;
-
-        [BsonElement("totalPrice")]
         private double _totalPrice;
-
-        [BsonElement("clientName")]
         private string _clientName;
-
-        [BsonElement("date")]
-        private DateTime _date;
-
-        [BsonElement("creationTime")]
         private DateTime _creationTime;
-
-        [BsonElement("endTime")]
         private DateTime _endTime;
-
-        [BsonElement("discounts")]
         private List<Discount> _discounts;
 
+
+
+        [BsonElement("id")]
+        public string Id { get { return _id; } }
+
+        [BsonElement("orderedShakes"), BsonRequired ]
         public List<OrderedShake> Shakes { get { return _shakes; } }
-        public string Name
-        {
-            get { return _clientName; }
-            set
-            {
-                if()
-            }
-        }
 
-        public string Id { get { return _id; } { set }
-        public double TotalPrice
-        {
-            get { return _totalPrice; }
-        }
+        [BsonElement("totalPrice")]
+        public double TotalPrice { get { return _totalPrice; } }
 
-        public string Id { get; internal set; }
+        [BsonElement("clientName"), BsonRequired, MaxLength(20)]
+        public string ClientName { get { return _clientName; } }
 
-        public Order(List<OrderedShake> shakes, double price, string clientName, DateTime date, List<Discount> discounts)
+        [BsonElement("creationTime"), BsonRequired]
+        public DateTime CreationTime { get { return _creationTime; } }
+
+        [BsonElement("endTime"), BsonRequired]
+        public DateTime Endtime { get { return _endTime; } }
+
+        [BsonElement("discounts")]
+        public List<Discount> Discount { get { return _discounts; } }
+
+
+        public Order(List<OrderedShake> shakes,  string clientName, DateTime creationTime, List<Discount> discounts)
         {
-            _id = new Guid().ToString();
+            _id = Guid.NewGuid().ToString();
             _shakes = shakes;
-            _totalPrice = price;
+            _totalPrice = shakes.Sum(x => x.price);
             _clientName = clientName;
-            _date = date;
+            _creationTime = DateTime.Now;
             _discounts = discounts;
         }
+
+      
     }
 }
