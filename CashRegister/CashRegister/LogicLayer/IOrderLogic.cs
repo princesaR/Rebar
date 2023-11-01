10c7d3acd1bcba8f9cd53ad8524567d0fba6bbdf
Reut <reutprober100@gmail.com>
2023-11-01 17:02:21 +0200

    Added logic layer.

---
+++ CashRegister/CashRegister/LogicLayer/IOrderLogic.cs
@@ -0,0 +1,14 @@
+﻿using CashRegister.Dto;
+using CashRegister.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashRegister.BeisnessLayer
+{
+    public interface IOrderLogic
+    {
+        Task<ActionResult<List<Order>>> Get();
+        Task<Order> Post(OrderDto orderDto);
+        Task<Order?> GetAsync(string id);
+
+    }
+}
