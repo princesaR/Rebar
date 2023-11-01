ae8e557fa6a416bdc1bffe5d621e4a136794ff02
Reut <reutprober100@gmail.com>
2023-11-01 16:59:38 +0200

    Add dto folder

---
+++ CashRegister/CashRegister/Dto/OrderDto.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using CashRegister.Models;
+
+namespace CashRegister.Dto
+{
+    public class OrderDto
+    {
+        private List<OrderedShakeDto> _shakes;
+        private string _name;
+        private List<DiscountDto> _discounts;
+
+        [Required(ErrorMessage = "must enter shake to the order")]
+        public List<OrderedShakeDto> Shakes
+        {
+            get { return _shakes; }
+            private set
+            {
+                if (value != null && value.Count() <= 10)
+                {
+                    _shakes = value;
+                }
+            }
+        }
+
+        [Required(ErrorMessage ="ClientName is required"), MaxLength(20)]
+        [RegularExpression(@"^[a-zA-Z ]+$", ErrorMessage = "Use letters only please")]
+        public string Name { get { return _name; } }
+        
+        
+        public List<DiscountDto> Discounts
+
+        {
+            get { return _discounts; }
+
+        }
+
+
+        public OrderDto(List<OrderedShakeDto> shakes, string name, List<DiscountDto> discounts)
+        {
+            _shakes = shakes;
+            _name = name;
+            _discounts = discounts;
+        }
+
+    }
+}
